aec93a77c3036224a965ffa337f87e8ce961976f
Tim Sleeper <mrt.sleeper@gmail.com>
2015-10-16 16:53:55 -0400

    Revert

--- templates/acl-3.0/index.php
+++ templates/acl-3.0/index.php
@@ -26,7 +26,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/acl-3.0/css/build/global.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jQuery.mmenu/5.3.4/css/jquery.mmenu.all.min.css">
 </head>
 <body>
 	<div id="fixed-navbar" class="nav">
@@ -44,15 +43,6 @@
                     </button>
                     <div id="navbar" class="collapse navbar-collapse">
                         <jdoc:include type="modules" name="nav" />
-            <div class="col-md-10">
-                <!--<div class="col-md-12">
-                    <p class="pull-right">847-836-8593 | internet@cp.allcreditlenders.net</p>
-                </div>-->
-                <div class="col-md-12">
-                    <div class="pull-right">
-                        <button class="m-btn">Members</button>
-                        <button class="a-btn">Apply Now!</button>
-                        <button class="btn btn-white mobile"><a href="#menu" data-icon="bars" class="mobilemenu">Menu</a></button>
                     </div>
                 </div>
             </div>
@@ -64,9 +54,6 @@
             </div>
         </div>
     </div>
-    <nav id="menu">
-        <jdoc:include type="modules" name="nav" />
-    </nav>
     <div class="container content">
         <div class="row">
             <div class="imgbar">
@@ -109,30 +96,5 @@
         });
     });
 </script>
-    <script type="text/javascript">
-            $(function() {
-                $('nav#menu').mmenu({
-                    extensions  : [ 'effect-slide-menu', 'pageshadow' ],
-                    searchfield : true,
-                    counters    : true,
-                    navbar      : {
-                        title       : 'Main Menu'
-                    },
-                    navbars     : [
-                        {
-                            position    : 'top',
-                            content     : [ 'searchfield' ]
-                        }, {
-                            position    : 'top',
-                            content     : [
-                                'prev',
-                                'title',
-                                'close'
-                            ]
-                        },
-                    ]
-                });
-            });
-    </script>
 </body>
 </html>
